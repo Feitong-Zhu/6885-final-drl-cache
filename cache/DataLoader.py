31ae83ff4d2dc621a50931af5a75ca0edaa8a959
Feitong-Zhu <114700600+Feitong-Zhu@users.noreply.github.com>
2023-12-17 11:00:10 +0800

    Update DataLoader.py

--- cache/DataLoader.py
+++ cache/DataLoader.py
@@ -12,23 +12,6 @@ class DataLoader(object):
     def get_operations(self):
         pass
 
-class DataLoaderPintos(DataLoader):
-    def __init__(self, progs, boot=False):
-        super(DataLoaderPintos, self).__init__()
-
-        if isinstance(progs, str): progs = [progs]
-        for prog in progs:
-            df = pd.read_csv(prog, header=0)
-            if not boot: df = df.loc[df['boot/exec'] == 1, :]
-            self.requests += list(df['blocksector'])
-            self.operations += list(df['read/write'])
-
-    def get_requests(self):
-        return self.requests
-
-    def get_operations(self):
-        return self.operations
-
 class DataLoaderZipf(DataLoader):
     def __init__(self, num_files, num_samples, param, num_progs=1, operation='random'):
         super(DataLoaderZipf, self).__init__()
